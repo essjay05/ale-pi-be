ad63f145abb4f8edf14b3e3a313794c02cdf2660
Joy Serquina <Joy.Serquina@scorpion.co>
2020-09-15 20:58:47 -0700

    Attempting to connect to MongoDb after installing on Windows

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require ('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = 4000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+// Connect to DB
+mongoose.connect('mongodb://127.00.0.1:27017/beers', { useNewUrlParser: true })
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log(`MongoDB database connection established successfully`);
+})
+
 // Port connection
 app.listen(PORT, function() {
     console.log(`Server is running on Port: ${PORT}`)
-})
+});
