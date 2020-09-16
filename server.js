2f36c49c49c1153203835b2aa04ca449179938fd
Joy Serquina <Joy.Serquina@scorpion.co>
2020-09-15 21:16:26 -0700

    Added error handling to MongoDB connection... need to provide MONGODB_URI value

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to DB
-mongoose.connect('mongodb://127.00.0.1:27017/beers', { useNewUrlParser: true })
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log(`MongoDB database connection established successfully`);
-})
+mongoose.connect('mongodb://localhost:27017/beers', { useNewUrlParser: true });
+// Error handling
+try {
+    mongoose.connect('mongodb://localhost/Ale-PI', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () => 
+        console.log(`MongoDB is successfully connected`));
+} catch (error) {
+    console.log(`ALERT: MongoDB could not connect ${error}`);
+}
 
 // Port connection
 app.listen(PORT, function() {
